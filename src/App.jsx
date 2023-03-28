af4955bb982b7f2b15ce7d1cc62aefb473aa5912
Evan <evan.lueber@stud.kbw.ch>
2023-03-28 11:05:37 +0200

    letze Änderungen vom 24.03.23

--- src/App.jsx
+++ src/App.jsx
@@ -16,44 +16,40 @@ function App() {
           <p className={step === 1 ? "current" : ""}>1</p>
           <p className={step === 2 ? "current" : ""}>2</p>
           <p className={step === 3 ? "current" : ""}>3</p>
-          <p className={step === 4 ? "current" : ""}>4</p>
+          <p className={step === 4 ? "current last" : "last"}>4</p>
         </div>
-        {
-        step === 1 ?
-          <>
-            <MyInput title="Geben Sie ihre Versicherungssumme ein" value={vsSumme} onChange={(e) => setVsSumme(e.target.value)} step={step} setStep={setStep}/>
-          </>
-          : 
-          step === 2?
-          <> 
-            <MyInput title="Geben Sie ihren Versicherungswert an" value={vsWert} onChange={(e) => setVsWert(e.target.value)} step={step} setStep={setStep} />
-          </>
-          :
-          step === 3 ?
-          <> 
-            <MyInput title="Geben Sie ihren Schaden an" value={schaden} onChange={(e) => setSchaden(e.target.value)} step={step} setStep={setStep}/>
-          </>
-          :
-          <> 
-            <p>Ihre Versicherung wird {Math.round((vsSumme / vsWert) * 100 * 100) / 100 >= 100 ? 100 : Math.round((vsSumme / vsWert) * 100 * 100) / 100}% des Schadens übernehmen</p>
-            <p>Ihre Versicherung wird {Math.round((vsSumme / vsWert) * schaden * 100) /100 >= schaden ? schaden : Math.round((vsSumme / vsWert) * schaden * 100) /100}CHF übernehmen</p>
-            <p className='werte'>Versicherungssumme: {vsSumme}</p>
-            <p className='werte'>Versicherungswert: {vsWert}</p>
-          </>
-        }  
+        <div id='inputKasten'>
+          {
+          step === 1 ?
+            <>
+              <MyInput title="Geben Sie ihre Versicherungssumme ein" value={vsSumme} onChange={(e) => setVsSumme(e.target.value)} step={step} setStep={setStep}/>
+            </>
+            :
+            step === 2?
+            <>
+              <MyInput title="Geben Sie ihren Versicherungswert an" value={vsWert} onChange={(e) => setVsWert(e.target.value)} step={step} setStep={setStep} />
+            </>
+            :
+            step === 3 ?
+            <>
+              <MyInput title="Geben Sie ihren Schaden an" value={schaden} onChange={(e) => setSchaden(e.target.value)} step={step} setStep={setStep}/>
+            </>
+            :
+            <>
+              <p>Ihre Versicherung wird {Math.round((vsSumme / vsWert) * 100 * 100) / 100 >= 100 ? 100 : Math.round((vsSumme / vsWert) * 100 * 100) / 100}% des Schadens übernehmen</p>
+              <p>Ihre Versicherung wird {Math.round((vsSumme / vsWert) * schaden * 100) /100 >= schaden ? schaden : Math.round((vsSumme / vsWert) * schaden * 100) /100}CHF übernehmen</p>
+              <p className='werte'>Versicherungssumme: {vsSumme}CHF</p>
+              <p className='werte'>Versicherungswert: {vsWert}CHF</p>
+              <p className='werte'>Schaden: {schaden}CHF</p>
+            </>
+          }
+        </div>  
 
         {step === 4 ?
-          <button onClick={()=>{setStep(1);setVsSumme("");setVsWert("")}}>Home</button>
+          <button id='home' onClick={()=>{setStep(1);setVsSumme("");setVsWert("")}}>Home</button>
         :
           ""
-        }
-
-        {step >= 2 && step < 4 ?
-          <button onClick={()=>setStep(step-1)}>Back</button>
-        :
-          ""
-        }
-          
+        }          
       </header>
     </div>
   );
--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -4,20 +4,27 @@ const MyInput = ({title, value, onChange, step, setStep}) => {
     return (
         <>
             <h2 className='inputTitle'>{title}</h2>
-            <input type="text" name="" id="" value={value} onChange={onChange}/>
+            <input type="text" name="" id="input" value={value} onChange={onChange}/>
 
-            {step < 4 ?
-            <button onClick={()=>{
-            if (value ==="" ){
-              alert("Geben Sie einen Wert in das Inputfeld ein")
-            }
-            else {
-              setStep(step+1)
-            }
-          }}>Next</button>
-        :
-          ""
-        } 
+            <div className="backNext">
+              {step >= 2 && step < 4 ?
+                  <button id='back' onClick={()=>setStep(step-1)}>Back</button>
+                :
+                  ""
+                }
+                {step < 4 ?
+                  <button id='next' onClick={()=>{
+                  if (value ==="" ){
+                    alert("Geben Sie einen Wert in das Inputfeld ein")
+                  }
+                  else {
+                    setStep(step+1)
+                  }
+                }}>Next</button>
+                :
+                  ""
+                }
+            </div>
         </>
     )
 }
